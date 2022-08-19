90ef7fcb3d0a6fba83389282c41e99c9010a310f
gustavojellav <111261566+gustavojellav@users.noreply.github.com>
2022-08-19 17:07:44 -0500

    Update blog.config.js

--- blog.config.js
+++ blog.config.js
@@ -1,35 +1,34 @@
 const BLOG = {
-  title: 'NextCraft',
-  author: 'Zuo Lan',
-  link: 'https://next-craft.vercel.app',
-  description: 'Using Craft.do pages as a blog',
+  title: 'Mis Notas Personales',
+  author: 'Gustavo Vallejo',
+  link: 'https://ideas.gustavovallejo.com',
+  description: 'Notas personales y secretas de Gustavo Vallejo',
   lang: 'en-US', // ['en-US', 'zh-CN', 'zh-HK', 'zh-TW']
   appearance: 'auto', // ['light', 'dark', 'auto'],
   font: 'sans-serif', // ['sans-serif', 'serif']
   lightBackground: '#FFFFFF', // use hex value, don't forget '#' e.g #fffefc
   darkBackground: '#222222', // use hex value, don't forget '#'
-  since: 2021, // If leave this empty, current year will be used.
-  showTitlebarText: false, // Craft Docs page show title bar text on desktop
+  since: 2022, // If leave this empty, current year will be used.
+  showTitlebarText: true, // Craft Docs page show title bar text on desktop
   autoCollapsedNavBar: false, // The automatically collapsed navigation bar
   ogImageGenerateURL: 'https://og-zl.vercel.app', // The link to generate OG image, don't end with a slash
-  craftConfigShareUrl: 'https://www.craft.do/s/8gQSdBtbuPjpp1', // The link to share your craft config
+  craftConfigShareUrl: 'https://www.craft.do/s/IsyIFpx0IGCjTL', // The link to share your craft config
   notesNav: {
-    index: 'Home',
-    blog: 'My Blog',
-    contact: 'Contact'
+    index: 'Inicio',
+    blog: 'Reto Kaizen',
+    contact: 'Universidad Superior'
   },
   notesLink: {
-    index: '/',
-    blog: 'https://zuolan.me/en',
-    contact: 'https://zuolan.me/en/contact'
+    index: 'http://ideas.gustavovallejo.com/post/notas-personales',
+    blog: 'https://retosuperior.com',
+    contact: 'https://universidadsuperior.com/platinum'
   },
   socialLink: {
-    twitter: 'https://twitter.com/izuolan',
-    github: 'https://github.com/izuolan',
-    telegram: 'https://t.me/zuolan'
+    twitter: 'https://twitter.com/gustavojellav',
+    telegram: 'https://t.me/+SfLwb_-EjHwcXZ-o'
   },
   seo: {
-    keywords: ['Blog', 'Craft.do', 'Craft Docs', 'Next.js', 'TailwindCSS']
+    keywords: ['Blog Hombre Superior', 'Gustavo Vallejo', 'Podcast Hombre Superior']
   },
   analytics: {
     provider: '', // Currently support Umami, fill with 'umami' to enable or leave it empty to disable it.
